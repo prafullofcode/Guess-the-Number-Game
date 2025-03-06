507f3696ce9f8721c83c47b6d2076b4141a38dca
prafullofcode <dahal.praphul268@gmail.com>
2025-03-06 21:23:46 +0545

    Alert message for smaller screens

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 let randomNumber = generateNumber();
 
 game();
@@ -6,26 +6,25 @@ function game() {
   let score = 20;
   let highScore = 0;
   randomNumber = generateNumber();
-  console.log(randomNumber);
-  updateTextContent('.score', `${score}`);
+  updateTextContent(".score", `${score}`);
 
-  document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
+  document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
     // when no input is given
     if (!guess) {
-      displayMessage('🙅 No number Entered');
+      displayMessage("🙅 No number Entered");
     }
     // when guessed correctly
     else if (guess === randomNumber) {
-      displayMessage('🎉🥳 You guessed Correctly');
+      displayMessage("🎉🥳 You guessed Correctly");
       correctGuess();
     }
     // when guessed incorrectly
     else {
       if (guess > randomNumber) {
-        displayMessage('⬇️ Guess a lower number');
+        displayMessage("⬇️ Guess a lower number");
       } else {
-        displayMessage('⬆️ Guess a higher number');
+        displayMessage("⬆️ Guess a higher number");
       }
       updateScore();
     }
@@ -33,19 +32,19 @@ function game() {
 
   function correctGuess() {
     // Updating "?"
-    updateTextContent('.number', randomNumber);
+    updateTextContent(".number", randomNumber);
     //Update css
 
     // document.querySelector('body').style.backgroundColor = '#60b347';
-    updateCssBackgroundColor('body', '#60b347');
+    updateCssBackgroundColor("body", "#60b347");
 
-    updateCssWidth('.number', '30rem');
+    updateCssWidth(".number", "30rem");
 
     //Update Highscore
     if (score >= highScore) {
       highScore = score;
     }
-    updateTextContent('.highscore', `${highScore}`);
+    updateTextContent(".highscore", `${highScore}`);
     return;
   }
 
@@ -53,10 +52,10 @@ function game() {
     score--;
     // when game is lost
     if (score <= 0) {
-      displayMessage('😭 You lost the game');
+      displayMessage("😭 You lost the game");
       score = 0;
     }
-    updateTextContent('.score', `${score}`);
+    updateTextContent(".score", `${score}`);
   }
 }
 
@@ -64,24 +63,24 @@ function generateNumber() {
   return Math.trunc(Math.random() * 20) + 1;
 }
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   resetGame();
 });
 
 function resetGame() {
-  document.querySelector('.guess').value = '';
+  document.querySelector(".guess").value = "";
 
-  displayMessage('Start guessing...');
+  displayMessage("Start guessing...");
 
-  updateCssBackgroundColor('body', '#222');
-  updateCssWidth('.number', '15rem');
+  updateCssBackgroundColor("body", "#222");
+  updateCssWidth(".number", "15rem");
 
-  updateTextContent('.number', '?');
+  updateTextContent(".number", "?");
   game();
 }
 
 function displayMessage(message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 }
 
 function updateTextContent(tagOrClassName, value) {
@@ -95,3 +94,17 @@ function updateCssBackgroundColor(tagOrClassName, value) {
 function updateCssWidth(tagOrClassName, value) {
   document.querySelector(tagOrClassName).style.width = value;
 }
+
+function checkScreenSize() {
+  if (window.innerWidth < 600) {
+    alert(
+      "This site + small screens = not a great match! For the best experience, use a bigger screen! 😉"
+    );
+  }
+}
+
+// Run when the page loads
+window.onload = checkScreenSize;
+
+// Run when the window is resized
+window.onresize = checkScreenSize;
